c47cce6b9d3ee46674fda7547c73a82c0754ef01
softchris <noringc@gmail.com>
2020-05-21 18:39:48 +0100

    adding eslint, closes #13

--- src/__tests__/server.js
+++ src/__tests__/server.js
@@ -1,74 +1,74 @@
-const { createServer, server, getHttpServer } = require('../server');
-const path = require('path');
-const fs = require('fs');
-const supertest = require('supertest');
+const { createServer, server, getHttpServer } = require("../server");
+const path = require("path");
+const fs = require("fs");
+const supertest = require("supertest");
 createServer(3000, path.join(__dirname, "db.yml"));
 
 const request = supertest(server);
 
-describe('server', () => {
+describe("server", () => {
   afterAll(async(done) => {
     // restore yml
-    const original = fs.readFileSync(path.join(__dirname, 'original-db.yml'));
-    fs.writeFileSync(path.join(__dirname, 'db.yml'), original);
+    const original = fs.readFileSync(path.join(__dirname, "original-db.yml"));
+    fs.writeFileSync(path.join(__dirname, "db.yml"), original);
     getHttpServer().close(() => {
       console.log("server closed!");
       done();
     });
-  })
+  });
 
-  test('should return products', async(done) => {
+  test("should return products", async(done) => {
     const products = [{
       id: 1,
-      name: 'tomato'
+      name: "tomato"
     }, {
       id: 2,
-      name: 'lettuce'
-    }]
+      name: "lettuce"
+    }];
 
-    const res = await request.get('/products')
+    const res = await request.get("/products");
     expect(res.status).toBe(200);
     expect(res.body).toEqual(products);
     done();
-  })
+  });
 
-  test('should filter by query parameters', async() => {
-    const firstItem = { id: 1, name: 'tomato' };
-    const secondItem = { id: 2, name: 'lettuce' };
+  test("should filter by query parameters", async() => {
+    const firstItem = { id: 1, name: "tomato" };
+    const secondItem = { id: 2, name: "lettuce" };
 
-    let res = await request.get('/products?page=1&pageSize=1');
+    let res = await request.get("/products?page=1&pageSize=1");
     let [ item ] = res.body;
-    expect(res.body.length).toBe(1);
+    expect(res.body).toHaveLength(1);
     expect(item).toEqual(firstItem);
 
-    res = await request.get('/products/?page=2&pageSize=1');
+    res = await request.get("/products/?page=2&pageSize=1");
     item = res.body[0];
-    expect(res.body.length).toBe(1);
+    expect(res.body).toHaveLength(1);
     expect(item).toEqual(secondItem);
-  })
+  });
 
-  test('should NOT respect filter when query param missing', async() => {
-    const res = await request.get('/products?page=1');
-    expect(res.body.length).toBe(2);
-  })
+  test("should NOT respect filter when query param missing", async() => {
+    const res = await request.get("/products?page=1");
+    expect(res.body).toHaveLength(2);
+  });
 
   test("should NOT respect filter when query param has wrong value type", async () => {
     const res = await request.get("/products?page=1&pageSize=abc");
-    expect(res.body.length).toBe(2);
+    expect(res.body).toHaveLength(2);
   });
 
-  test('should return intro text on default route', async() => {
-    const res = await request.get('/');
-    expect(res.text).toMatch(/Welcome to YAML Server/)
-  })
+  test("should return intro text on default route", async() => {
+    const res = await request.get("/");
+    expect(res.text).toMatch(/Welcome to YAML Server/);
+  });
 
-  test('should return a product', async(done) => {
-    const product = { id: 1, name: 'tomato' };
-    const res = await request.get('/products/1')
+  test("should return a product", async(done) => {
+    const product = { id: 1, name: "tomato" };
+    const res = await request.get("/products/1");
     expect(res.status).toBe(200);
     expect(res.body).toEqual(product);
     done();
-  })
+  });
 
   test("should return 404 resource not found", async (done) => {
     const res = await request.get("/products/3");
@@ -76,78 +76,78 @@ describe('server', () => {
     done();
   });
 
-  test('should add product to /products', async(done) => {
-    const createdRecord = { id: 3, name: 'cucumber' };
+  test("should add product to /products", async(done) => {
+    const createdRecord = { id: 3, name: "cucumber" };
 
     const res = await request
-      .post('/products')
-      .send({ name : 'cucumber' })
-    expect(res.status).toBe(201)
-    expect(res.body).toEqual(createdRecord)
+      .post("/products")
+      .send({ name : "cucumber" });
+    expect(res.status).toBe(201);
+    expect(res.body).toEqual(createdRecord);
     done();
-  })
+  });
 
-  test('should update product', async(done) => {
-    const changeTo = { id : 3, name: 'gurkin' };
+  test("should update product", async(done) => {
+    const changeTo = { id : 3, name: "gurkin" };
     let res = await request
-      .put('/products')
-      .send({ id: 3, name: 'gurkin' })
-    expect(res.status).toBe(200)
+      .put("/products")
+      .send({ id: 3, name: "gurkin" });
+    expect(res.status).toBe(200);
     expect(res.body).toEqual(changeTo);
 
-    res = await request.get('/products/3')
+    res = await request.get("/products/3");
     expect(changeTo).toEqual(res.body);
     done();
-  })
+  });
 
-  test('should return 404 and error message when trying to update non existing item', async() => {
-    const nonExistingItem = { id: 99, name: 'unknown' };
+  test("should return 404 and error message when trying to update non existing item", async() => {
+    const nonExistingItem = { id: 99, name: "unknown" };
     let res = await request
-      .put('/products')
-      .send(nonExistingItem)
+      .put("/products")
+      .send(nonExistingItem);
     expect(res.status).toBe(404);
-    expect(res.text).toBe("Item not found with ID" + nonExistingItem.id)
-  })
+    expect(res.text).toBe("Item not found with ID" + nonExistingItem.id);
+  });
 
-  test('should delete product', async(done) => {
+  test("should delete product", async(done) => {
     const deletedItem = { id: 3, name: "gurkin" };
-    let res = await request.delete('/products/3');
-    expect(res.status).toBe(200)
-    expect(res.body).toEqual(deletedItem)
+    let res = await request.delete("/products/3");
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual(deletedItem);
 
-    res = await request.get('/products/3');
-    expect(res.status).toBe(404)
+    res = await request.get("/products/3");
+    expect(res.status).toBe(404);
     done();
-  })
+  });
 
-  test('should respect existing schema on POST', async() => {
+  test("should respect existing schema on POST", async() => {
     let res = await request
-      .post('/products')
-      .send({ title: 'should not work' })
+      .post("/products")
+      .send({ title: "should not work" });
     
-    expect(res.status).toBe(400)
-  })
+    expect(res.status).toBe(400);
+  });
 
-  test('should create a new resource based on a /<resource>/new/ call', async() => {
-    const kitten = { id: 1, title: 'paw paw' };
+  test("should create a new resource based on a /<resource>/new/ call", async() => {
+    const kitten = { id: 1, title: "paw paw" };
     let res = await request
-      .post('/kittens/new')
-      .send({ title: 'paw paw' });
+      .post("/kittens/new")
+      .send({ title: "paw paw" });
 
     expect(res.status).toBe(201);  
     expect(res.body).toEqual(kitten);
 
-    res = await request.get('/kittens');
+    res = await request.get("/kittens");
     expect(res.status).toBe(200);
     expect(res.body).toEqual(kitten);
-  })
+  });
 
   test("should NOT create a new resource /<resource>/new/ call when resource already exist", async() => {
     let res = await request
       .post("/kittens/new")
       .send({ title: "paw paw" });
 
-    expect(res.status).toBe(400)
-    expect(res.text).toBe(`/kittens already exist`);
+    expect(res.status).toBe(400);
+    expect(res.text).toBe("/kittens already exist");
   });
-})
+});
