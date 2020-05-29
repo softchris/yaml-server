371a0610dad7c844deb0cea13e65cb3310a6ff42
softchris <noringc@gmail.com>
2020-05-29 09:58:59 +0100

    closes #28

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 #!/usr/bin/env node
 const argv = require("yargs").argv;
+const fs = require('fs');
+const path = require('path');
 
-const { createServer } = require("./server");
+const { createServer, getHttpServer } = require("./server");
 const { dbPath } = require("./config")("db.yml");
 
 const port = argv.port || 3000;
 const database = argv.database || dbPath;
 const autoStart = argv.autoStart === 'off' ? false : true;
+const hotReload = argv.hotReload === "off" ? false : true;
 
 createServer(port, database, argv.static, autoStart);
 
-
+if (hotReload) {
+  fs.watchFile(database, (curr, prev) => {
+    console.log("Database changed");
+    getHttpServer().close(() => {
+      console.log("Restarting the server");
+      createServer(port, database, argv.static, autoStart);
+    });
+  });
+}
+  
 
